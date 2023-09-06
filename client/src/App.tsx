bd0f62da6aef6c562d1c706f3d74d04ed535207a
kostili-tec <weh4teyou@gmail.com>
2023-09-06 17:36:05 +0300

    feat: add three libs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
+import { Canvas } from '@react-three/fiber';
+
 import './styles/index.scss';
+import { Box } from './components/Box';
+import { ConeMesh } from './components/Cone';
+import { OrbitControls } from '@react-three/drei';
 
 const App = () => {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <Canvas style={{ height: 500 }}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        {/* <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} /> */}
+        <ConeMesh />
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/Box.tsx
@@ -0,0 +1,24 @@
+import * as THREE from 'three';
+import { useRef, useState } from 'react';
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
+
+export const Box = (props: ThreeElements['mesh']) => {
+  const meshRef = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  const { position } = props;
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+};
