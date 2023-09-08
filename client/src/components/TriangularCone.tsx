2c75e8f5053c30e5902bd26e8ac4b0fae7cf4408
kostili-tec <weh4teyou@gmail.com>
2023-09-08 19:26:22 +0300

    refactor: rerender cone by useEffect

--- client/src/components/TriangularCone.tsx
+++ client/src/components/TriangularCone.tsx
@@ -1,37 +1,21 @@
-import { FC, useRef, useState } from 'react';
-import { useFrame } from '@react-three/fiber';
+import { FC, useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 
 import { IConeCalculatedData as TriangularConeProps } from '../types/types';
 
-interface ConeViewerProps {
-  height: number;
-  radius: number;
-  segments: number;
-}
-
 export const TriangularCone: FC<TriangularConeProps> = ({ indices, vertices }) => {
   const meshRef = useRef<THREE.Mesh>();
+  const [geometry, setGeometry] = useState<THREE.BufferGeometry | null>(null);
 
-  /*   useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
-    }
-  }); */
-
-  // Создаем геометрию и материал
-  const geometry = new THREE.BufferGeometry();
-  geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3));
-  geometry.setIndex(indices);
-
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-  console.log('vertices', vertices);
-  console.log('indices', indices);
+  useEffect(() => {
+    const newGeometry = new THREE.BufferGeometry();
+    newGeometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3));
+    newGeometry.setIndex(indices);
+    setGeometry(newGeometry);
+  }, [indices, vertices]);
 
   return (
-    <mesh geometry={geometry} material={material} ref={meshRef}>
+    <mesh geometry={geometry} ref={meshRef}>
       <meshBasicMaterial attach="material" color={0xa52a2a} wireframe={true} />
     </mesh>
   );
